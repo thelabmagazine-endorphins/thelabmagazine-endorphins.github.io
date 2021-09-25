dfd88bd37c48b955a242496457286dd4950e013a
thelabmagazine-endorphins <91334988+thelabmagazine-endorphins@users.noreply.github.com>
2021-09-25 16:30:03 +0700

    Add files via upload

---
+++ endorphin.js
@@ -0,0 +1,20 @@
+
+var nameHtml = prompt("Enter your name: ");
+function init(){
+	var ders = document.getElementById("name")
+	ders.innerHTML = "Hello " + nameHtml + "<p>Thanks for coming here.I hope you will enjoy these knowledges. While reading the website, listen to the music as below &hearts;: </p>";
+	if (nameHtml == "Nga"){
+		ders.innerHTML = "I love you";
+	}
+	var btn = document.getElementById("btn");
+	btn.onclick = convertQuote;
+}
+
+function convertQuote(){
+	var quote = document.getElementById("quote");
+	quote.innerHTML = "<strong>“If you don’t make time for exercise, you’ll probably have to make time for illness.” - Robin Sharma</strong>"
+	var thank = document.getElementById("btn");
+	thank.innerHTML = "Thanks " + nameHtml;
+}
+
+window.onload = init;
